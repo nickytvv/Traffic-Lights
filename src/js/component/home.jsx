e0b29f5a7b47c98e0108d84d3b818dd7f12b898d
Nicholas Sanchez <nickasanchez011@gmail.com>
2023-03-29 14:24:29 +0000

    Updated working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+  const [color, setColor] = useState("red");
 
-//create your first component
+  const handleClick = (selectedColor) => {
+    setColor(selectedColor);
+  };
+
+  const redClass = color === "red" ? "selected" : "";
+  const yellowClass = color === "yellow" ? "selected" : "";
+  const greenClass = color === "green" ? "selected" : "";
+
+  return (
+    <div className="traffic-light">
+      <div
+        className={"light red " + redClass}
+        onClick={() => handleClick("red")}
+      />
+      <div
+        className={"light yellow " + yellowClass}
+        onClick={() => handleClick("yellow")}
+      />
+      <div
+        className={"light green " + greenClass}
+        onClick={() => handleClick("green")}
+      />
+    </div>
+  );
+};
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [showTrafficLight, setShowTrafficLight] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowTrafficLight(!showTrafficLight);
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Traffic Light App</h1>
+      {showTrafficLight ? <TrafficLight /> : null}
+      <button className="btn btn-success" onClick={handleButtonClick}>
+        {showTrafficLight ? "Hide" : "Show"} Traffic Light
+      </button>
+    </div>
+  );
 };
 
 export default Home;
